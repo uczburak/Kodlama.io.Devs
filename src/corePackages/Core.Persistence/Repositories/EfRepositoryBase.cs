b335d03880d492637b112e6407272174a167c740
uczburak <uczbrk@gmail.com>
2022-09-03 23:06:26 +0300

    Add project files.

---
+++ src/corePackages/Core.Persistence/Repositories/EfRepositoryBase.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories
+{
+    public class EfRepositoryBase<TEntity, TContext> : IRepository<TEntity> 
+        where TEntity : class
+        where TContext : DbContext
+    {
+        private readonly DbContext Context;
+
+        public EfRepositoryBase(TContext context)
+        {
+            Context = context;
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Added;
+            Context.SaveChanges();
+            return entity as TEntity;
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.SaveChanges();
+            return entity as TEntity;
+        }
+
+        public TEntity Delete(TEntity entity)
+        {
+            Context.Entry(Context).State = EntityState.Deleted;
+            Context.SaveChanges();
+            return entity as TEntity;
+        }
+
+        public TEntity? Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().FirstOrDefault(predicate);
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return filter != null ? Context.Set<TEntity>().Where(filter).ToList() : Context.Set<TEntity>().ToList();
+        }
+
+        public IQueryable<TEntity> Query()
+        {
+            return Context.Set<TEntity>();
+        }
+
+        
+    }
+}
---
+++ src/corePackages/Core.Persistence/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories
+{
+    public interface IRepository<T> : IQuery<T> where T : class
+    {
+        T Add(T entity);
+        T Update(T entity);
+        T Delete(T entity);
+        T Get(Expression<Func<T, bool>> predicate);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+    }
+}
